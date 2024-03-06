2858af89fb2268106b48276e66a8123e851f68b5
Aleksei Ganikhin <tomatetz@gmail.com>
2024-03-06 17:26:09 +0100

    add chart gradient

--- src/components/HomePage/Chart/ChartComponent.jsx
+++ src/components/HomePage/Chart/ChartComponent.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import moment from 'moment';
 
 import {
+  Area,
   CartesianGrid,
+  ComposedChart,
+  Line,
   ResponsiveContainer,
   Scatter,
   ScatterChart,
@@ -14,11 +17,11 @@ import { ChartTooltip, ChartTooltipPeriod, ChartTooltipRow } from './chart.style
 
 export const LineChart = ({ data }) => (
   <ResponsiveContainer width="95%" height={370}>
-    <ScatterChart>
+    <ComposedChart data={data}>
       <defs>
         <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-          <stop offset="5%" stopColor="#129a74" stopOpacity={0.1} />
-          <stop offset="95%" stopColor="#FFFFFF" stopOpacity={0.1} />
+          <stop offset="5%" stopColor="#19c785" stopOpacity={0.4} />
+          <stop offset="95%" stopColor="#2d3042" stopOpacity={0.1} />
         </linearGradient>
       </defs>
       <CartesianGrid vertical={false} stroke="#2d3042" strokeDasharray={'10 3'} />
@@ -38,17 +41,28 @@ export const LineChart = ({ data }) => (
         tickFormatter={() => ''}
         hide
       />
-      <Scatter
-        data={data}
-        fill="#19c785"
-        line={{ stroke: '#19c785', strokeWidth: '2px' }}
-        lineJointType="monotoneX"
-        lineType="joint"
-        name="amount"
-        shape={<RenderDot />}
+      <Line
+        type="monotone"
+        strokeLinecap="round"
+        strokeWidth={2}
+        dataKey="amount"
+        stroke="#19c785"
+        dot={(data) => <RenderDot data={data} key={data.cx} />}
+        legendType="none"
+        activeDot={false}
+      />
+      <Area
+        type="monotone"
+        dataKey="amount"
+        stroke=""
+        strokeWidth={2}
+        fillOpacity={1}
+        fill="url(#colorUv)"
+        legendType="none"
+        activeDot={false}
       />
       <Tooltip content={<CustomTooltip />} cursor={false} />
-    </ScatterChart>
+    </ComposedChart>
   </ResponsiveContainer>
 );
 
@@ -58,14 +72,16 @@ const CustomTooltip = ({ active, payload }) => {
     if (!timeValue.payload.logo) return null;
     const { company, title, period } = timeValue.payload;
     return (
-      <ChartTooltip>
-        <ChartTooltipRow>
-          <img src={timeValue.payload.logo} width="24px" alt="" />
-          <b>{company}</b>
-        </ChartTooltipRow>
-        <ChartTooltipRow className="mt-2">{title}</ChartTooltipRow>
-        <ChartTooltipPeriod>{period}</ChartTooltipPeriod>
-      </ChartTooltip>
+      <>
+        <ChartTooltip>
+          <ChartTooltipRow>
+            <img src={timeValue.payload.logo} width="24px" alt="" />
+            <b>{company}</b>
+          </ChartTooltipRow>
+          <ChartTooltipRow className="mt-2">{title}</ChartTooltipRow>
+          <ChartTooltipPeriod>{period}</ChartTooltipPeriod>
+        </ChartTooltip>
+      </>
     );
   }
 
@@ -73,8 +89,12 @@ const CustomTooltip = ({ active, payload }) => {
 };
 
 const RenderDot = (data) => {
-  const { cx, cy, logo } = data;
-  if (!data.logo) return null;
+  const {
+    cx,
+    cy,
+    payload: { logo },
+  } = data.data;
+  if (!logo) return null;
   return (
     <g>
       <circle cx={cx} cy={cy - 15} r={17} fill="#fff" />
